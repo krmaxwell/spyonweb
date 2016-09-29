7b32668c95c929ae70f2b699b7ab33950dbe4007
Kyle Maxwell <kmaxwell@verisign.com>
2016-09-28 23:27:28 -0400

    Implement other API calls

--- debugTRX_Server.py
+++ debugTRX_Server.py
@@ -8,6 +8,8 @@ from spyonweb import Spyonweb
 
 ValidHostnameRegex = "^(([a-zA-Z0-9]|[a-zA-Z0-9][a-zA-Z0-9\-]*[a-zA-Z0-9])\.)*([A-Za-z0-9]|[A-Za-z0-9][A-Za-z0-9\-]*[A-Za-z0-9])$"
 ValidIpAddressRegex = "^(([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])\.){3}([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])$"
+ValidAdsenseRegex = "^pub-[0-9]+$"
+ValidAnalyticsRegex = "^UA-[0-9]+$"
 
 # dispatcher
 
@@ -16,13 +18,107 @@ ValidIpAddressRegex = "^(([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])\.){3}(
 def summary():
     if request.body.len > 0:
         incoming = TRX.MaltegoMsg(request.body.getvalue())
-        if (incoming.Type == "maltego.Domain" or incoming.Type == "maltego.DNSName") and re.match(ValidHostnameRegex, incoming.Value):
+        if (incoming.Type == "Domain" or incoming.Type == "DNSName") and re.match(ValidHostnameRegex, incoming.Value):
             s = Spyonweb(incoming.TransformSettings['api'])
             data = s.summary(incoming.Value)
             return process_summary(data)
         else:
             xform = TRX.MaltegoTransform()
             xform.addException("Must submit a valid host name or domain name")
+            return xform.throwExceptions()
+
+
+@route('/domain')
+def domain():
+    if request.body.len > 0:
+        incoming = TRX.MaltegoMsg(request.body.getvalue())
+        if (incoming.Type == "Domain" or incoming.Type == "DNSName") and re.match(ValidHostnameRegex, incoming.Value):
+            s = Spyonweb(incoming.TransformSettings['api'])
+            data = s.domain(incoming.Value)
+            return process_domain(data)
+        else:
+            xform = TRX.MaltegoTransform()
+            xform.addException("Must submit a valid host name or domain name")
+            return xform.throwExceptions()
+
+
+@route('/adsense')
+def adsense():
+    if request.body.len > 0:
+        incoming = TRX.MaltegoMsg(request.body.getvalue())
+        xform = TRX.MaltegoTransform()
+        if incoming.Type == "Phrase" and re.match(ValidAdsenseRegex, incoming.Value):
+            s = Spyonweb(incoming.TransformSettings['api'])
+            data = s.adsense(incoming.Value, limit=incoming.Slider)
+            for name in data:
+                ent = xform.addEntity("maltego.Domain", name)
+                ent.setLinkLabel(data[name])  # date ID was associated with domain
+        else:
+            xform.addException("Must submit a valid Adsense publisher ID")
+            return xform.throwExceptions()
+
+
+@route('/analytics')
+def analytics():
+    if request.body.len > 0:
+        incoming = TRX.MaltegoMsg(request.body.getvalue())
+        xform = TRX.MaltegoTransform()
+        if incoming.Type == "Phrase" and re.match(ValidAnalyticsRegex, incoming.Value):
+            s = Spyonweb(incoming.TransformSettings['api'])
+            data = s.analytics(incoming.Value, limit=incoming.Slider)
+            for name in data:
+                ent = xform.addEntity("maltego.Domain", name)
+                ent.setLinkLabel(data[name])  # date ID was associated with domain
+        else:
+            xform.addException("Must submit a valid Analytics tracking ID")
+            return xform.throwExceptions()
+
+
+@route('/ip')
+def ip():
+    if request.body.len > 0:
+        incoming = TRX.MaltegoMsg(request.body.getvalue())
+        xform = TRX.MaltegoTransform()
+        if incoming.Type == "IPv4Address" and re.match(ValidIpAddressRegex, incoming.Value):
+            s = Spyonweb(incoming.TransformSettings['api'])
+            data = s.ipaddress(incoming.Value, limit=incoming.Slider)
+            for name in data:
+                ent = xform.addEntity("maltego.Domain", name)
+                ent.setLinkLabel(data[name])  # date IP address was associated with domain
+        else:
+            xform.addException("Must submit a valid IPv4 address")
+            return xform.throwExceptions()
+
+
+@route('/dns_domain')
+def dns_domain():
+    if request.body.len > 0:
+        incoming = TRX.MaltegoMsg(request.body.getvalue())
+        xform = TRX.MaltegoTransform()
+        if (incoming.Type == "Domain" or incoming.Type == "DNSName" or incoming.Type == "NSRecord") and re.match(ValidHostnameRegex, incoming.Value):
+            s = Spyonweb(incoming.TransformSettings['api'])
+            data = s.dns_domain(incoming.Value, limit=incoming.Slider)
+            for name in data:
+                ent = xform.addEntity("maltego.Domain", name)
+                ent.setLinkLabel(data[name])  # date domain name was associated with server
+        else:
+            xform.addException("Must submit a valid host name")
+            return xform.throwExceptions()
+
+
+@route('/ip_dns')
+def ip_dns():
+    if request.body.len > 0:
+        incoming = TRX.MaltegoMsg(request.body.getvalue())
+        xform = TRX.MaltegoTransform()
+        if incoming.Type == "IPv4Address" and re.match(ValidIpAddressRegex, incoming.Value):
+            s = Spyonweb(incoming.TransformSettings['api'])
+            data = s.ip_dns(incoming.Value, limit=incoming.Slider)
+            for name in data:
+                ent = xform.addEntity("maltego.NSRecord", name)
+                ent.setLinkLabel(data[name])  # date domain name was associated with server
+        else:
+            xform.addException("Must submit a valid host name")
             return xform.throwExceptions()
 
 
@@ -46,6 +142,27 @@ def process_summary(data):
     for ip in data['result']['summary'][name]['items'].get('ip', ''):
         ent = xform.addEntity("maltego.IPv4Address", ip)
         ent.setWeight(data['result']['summary'][name]['items']['ip'][ip])
+    return xform.returnOutput()
+
+
+def process_domain(data):
+    xform = TRX.MaltegoTransform()
+    if data['status'] != 'found':
+        xform.addUIMessage("No results found", TRX.UIM_FATAL)
+        return xform.returnOutput()
+    name = data['result']['domain'].keys()[0]
+    for code in data['result']['domain'][name]['items'].get('adsense', ''):
+        ent = xform.addEntity("maltego.Phrase", code)
+        ent.setWeight(data['result']['domain'][name]['items']['adsense'][code])
+    for code in data['result']['domain'][name]['items'].get('analytics', ''):
+        ent = xform.addEntity("maltego.Phrase", code)
+        ent.setWeight(data['result']['domain'][name]['items']['analytics'][code])
+    for server in data['result']['domain'][name]['items'].get('dns_servers', ''):
+        ent = xform.addEntity("maltego.NSRecord", server)
+        # TODO: how to represent IP addresses returned in this API call?
+    for ip in data['result']['domain'][name]['items'].get('ip', ''):
+        ent = xform.addEntity("maltego.IPv4Address", ip)
+        ent.setWeight(data['result']['domain'][name]['items']['ip'][ip])
     return xform.returnOutput()
 
 
